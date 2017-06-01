29ef2c7c6ddd1dc805ce0ffe736c245268fe0b2a
Marko <Marko@PPIRC_SLO>
2017-06-01 17:46:32 +0200

    feat (StatefulWebApiProd.cs) : try stateful scenario

---
+++ StatefulWebApiProd/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatefulWebApiProd.Models
+{
+  public class Product
+  {
+    public int ProductID { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+  }
+}
